5f28f46931659e8115f4f00e4e992cd980906b63
zAndrezz <84895875+zAndrezz@users.noreply.github.com>
2021-11-09 15:03:53 -0600

    working in 4.4
    ''

---
+++ src/NumberOfEvents.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+
+
+class NumberOfEvents extends Component {
+
+  state = {
+    numberOfEvents: 15,
+  }
+
+  handleInputChanged = (event) => {
+    const number = event.target.value;
+    this.setState({ 
+      numberOfEvents: number,
+    });
+    this.props.updateEventCount(event.target.value);
+  };
+
+  render() {
+    return (
+      <div>
+        <p>Limit search results </p><input type="number" id="numberInput" value={this.state.numberOfEvents} className="numberInput" onChange={(e) => this.handleInputChanged(e)} />
+      </div>
+    );
+  }
+}
+
+
+export default NumberOfEvents;
---
+++ src/__tests__/NumberOfEvents.test.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import NumberOfEvents from '../NumberOfEvents';
+
+
+describe('<NumberOfEvents /> component', () => {
+
+  let NumberOfEventsWrapper;
+  beforeAll(() => {
+    NumberOfEventsWrapper = shallow(<NumberOfEvents />);
+  });
+
+  test('render text input', () => {
+    expect(NumberOfEventsWrapper.find('.numberInput')).toHaveLength(1);
+  });
+
+  test('render text input correcty', () => {
+    const numberOfEvents = NumberOfEventsWrapper.state('numberOfEvents');
+    expect(NumberOfEventsWrapper.find('.numberInput').prop('value')).toBe(numberOfEvents);
+  });
+
+  // test('change state when text input changes', () => {
+  //   NumberOfEventsWrapper.setState({
+  //     numberOfEvents: '5'
+  //   });
+  //   const eventObject = { target: { value: '2' }};
+  //   NumberOfEventsWrapper.find('.numberInput').simulate('change', eventObject);
+  //   expect(NumberOfEventsWrapper.state('numberOfEvents')).toBe('2');
+  // });
+
+});
