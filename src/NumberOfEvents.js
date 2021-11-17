913969808959f76238730aba478702a32f09b272
zAndrezz <84895875+zAndrezz@users.noreply.github.com>
2021-11-17 14:27:39 -0600

    fixing style

--- src/NumberOfEvents.js
+++ src/NumberOfEvents.js
@@ -5,7 +5,7 @@ class NumberOfEvents extends Component {
   render() {
     return (
       <div className="NumberOfEvents mt-40">
-        <h3>How Many Events?</h3>
+        <p>How Many Events?</p>
 
         <RangeSlider
           min={0}
