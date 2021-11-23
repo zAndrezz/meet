23d036b1b3cd3780ae796d5bada86ac949ef8383
zAndrezz <84895875+zAndrezz@users.noreply.github.com>
2021-11-22 19:03:39 -0600

    Update api.js

--- src/api.js
+++ src/api.js
@@ -28,6 +28,10 @@ const checkToken = async (accessToken) => {
 export const getEvents = async () => {
   NProgress.start();
 
+  if (window.location.href.startsWith("http://localhost")) {
+    NProgress.done();
+    return mockData;
+  }
   if (!navigator.onLine) {
     const data = localStorage.getItem("lastEvents");
     NProgress.done();
